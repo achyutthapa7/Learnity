5fd50a28edc7b02429672eecd92c41e2d26bb8d4
achyutthapa7 <achyut777thapa@gmail.com>
2023-09-09 16:21:00 +0545

    dfs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/Learnity/",
+  base: "https://github.com/achyutthapa7/Learnity/",
 });
